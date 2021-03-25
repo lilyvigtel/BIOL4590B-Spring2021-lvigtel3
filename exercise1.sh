ee221edcbd557ff3cecf56bd5b5141edf11c3193
lilyvigtel <81382165+lilyvigtel@users.noreply.github.com>
2021-03-25 13:47:09 -0400

    Upload files

---
+++ exercise1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo Hello world!
+echo Hello world!
+echo Hello world!
+echo Hello world!
+echo Hello world!
+echo Hello world!
+echo Hello world!
+echo Hello world!
+echo Hello world!
+echo Hello world!
+
+sleep 2m
+
+ls -a ~ | wc -l > exercise1.txt
